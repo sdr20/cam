38ec150b105ef2b03afd634bb18a58f3573ad86a
Steven DR <145115889+sdr20@users.noreply.github.com>
2024-09-25 21:51:53 +0800

    Update app_httpd.cpp

--- ESP32_Camera_4WD_Robot_Car/app_httpd.cpp
+++ ESP32_Camera_4WD_Robot_Car/app_httpd.cpp
@@ -1,14 +1,9 @@
-// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
-//            or another board which has PSRAM enabled
-// Adafruit ESP32 Feather
 #include "esp_http_server.h"
 #include "esp_timer.h"
 #include "esp_camera.h"
 #include "img_converters.h"
 #include "camera_index.h"
 #include "Arduino.h"
-
-
 
 #define LEFT_M0     13
 #define LEFT_M1     12
@@ -19,7 +14,7 @@ int noStop = 0;
 const int freq = 2000;
 const int motorPWMChannnel = 8;
 const int lresolution = 8;
-volatile unsigned int  motor_speed   = 100;
+volatile unsigned int motor_speed = 100;
 void robot_setup();
 void robot_stop();
 void robot_fwd();
@@ -28,6 +23,8 @@ void robot_left();
 void robot_right();
 volatile unsigned long previous_time = 0;
 volatile unsigned long move_interval = 250;
+bool live_feed_enabled = true; // Toggle for live feed on/off
+
 unsigned int get_speed(unsigned int sp)
 {
   return map(sp, 0, 100, 0, 255);
@@ -53,7 +50,6 @@ void robot_setup()
 void update_speed()
 {  
     ledcWrite(motorPWMChannnel, get_speed(motor_speed));
-    
 }
 
 void robot_stop()
@@ -96,9 +92,7 @@ void robot_left()
   ledcWrite(6, speed);
 }
 
-
- extern int gpLed;
-
+extern int gpLed;
 extern String WiFiAddr;
 
 void WheelAct(int nLf, int nLb, int nRf, int nRb);
@@ -197,6 +191,10 @@ static esp_err_t capture_handler(httpd_req_t *req){
 }
 
 static esp_err_t stream_handler(httpd_req_t *req){
+    if (!live_feed_enabled) {
+        return httpd_resp_send(req, "Live feed is off", HTTPD_RESP_USE_STRLEN);
+    }
+
     camera_fb_t * fb = NULL;
     esp_err_t res = ESP_OK;
     size_t _jpg_buf_len = 0;
@@ -213,7 +211,7 @@ static esp_err_t stream_handler(httpd_req_t *req){
         return res;
     }
 
-    while(true){
+    while(live_feed_enabled){
         fb = esp_camera_fb_get();
         if (!fb) {
             Serial.printf("Camera capture failed");
@@ -254,356 +252,88 @@ static esp_err_t stream_handler(httpd_req_t *req){
             break;
         }
         int64_t fr_end = esp_timer_get_time();
-
         int64_t frame_time = fr_end - last_frame;
         last_frame = fr_end;
         frame_time /= 1000;
-        uint32_t avg_frame_time = ra_filter_run(&ra_filter, frame_time);
-        Serial.printf("MJPG: %uB %ums (%.1ffps), AVG: %ums (%.1ffps)"
-            ,(uint32_t)(_jpg_buf_len),
-            (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time,
-            avg_frame_time, 1000.0 / avg_frame_time
-        );
+        Serial.printf("MJPG: %uB %ums (%.1ffps)\n",
+            (uint32_t)(_jpg_buf_len),
+            (uint32_t)frame_time, 1000.0 / (uint32_t)frame_time);
     }
 
     last_frame = 0;
     return res;
 }
 
-static esp_err_t cmd_handler(httpd_req_t *req){
-    char*  buf;
-    size_t buf_len;
-    char variable[32] = {0,};
-    char value[32] = {0,};
-
-    buf_len = httpd_req_get_url_query_len(req) + 1;
-    if (buf_len > 1) {
-        buf = (char*)malloc(buf_len);
-        if(!buf){
-            httpd_resp_send_500(req);
-            return ESP_FAIL;
-        }
-        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
-            if (httpd_query_key_value(buf, "var", variable, sizeof(variable)) == ESP_OK &&
-                httpd_query_key_value(buf, "val", value, sizeof(value)) == ESP_OK) {
-            } else {
-                free(buf);
-                httpd_resp_send_404(req);
-                return ESP_FAIL;
-            }
-        } else {
-            free(buf);
-            httpd_resp_send_404(req);
-            return ESP_FAIL;
-        }
-        free(buf);
-    } else {
-        httpd_resp_send_404(req);
+// Toggle live feed on/off
+static esp_err_t toggle_feed_handler(httpd_req_t *req) {
+    char buf[5];
+    int ret = httpd_req_recv(req, buf, sizeof(buf));
+    if (ret <= 0) {
         return ESP_FAIL;
     }
 
-    int val = atoi(value);
-    sensor_t * s = esp_camera_sensor_get();
-    int res = 0;
-
-    if(!strcmp(variable, "framesize")) {
-        if(s->pixformat == PIXFORMAT_JPEG) res = s->set_framesize(s, (framesize_t)val);
-    }
-    else if(!strcmp(variable, "quality")) res = s->set_quality(s, val);
-    else if(!strcmp(variable, "contrast")) res = s->set_contrast(s, val);
-    else if(!strcmp(variable, "brightness")) res = s->set_brightness(s, val);
-    else if(!strcmp(variable, "saturation")) res = s->set_saturation(s, val);
-    else if(!strcmp(variable, "gainceiling")) res = s->set_gainceiling(s, (gainceiling_t)val);
-    else if(!strcmp(variable, "colorbar")) res = s->set_colorbar(s, val);
-    else if(!strcmp(variable, "awb")) res = s->set_whitebal(s, val);
-    else if(!strcmp(variable, "agc")) res = s->set_gain_ctrl(s, val);
-    else if(!strcmp(variable, "aec")) res = s->set_exposure_ctrl(s, val);
-    else if(!strcmp(variable, "hmirror")) res = s->set_hmirror(s, val);
-    else if(!strcmp(variable, "vflip")) res = s->set_vflip(s, val);
-    else if(!strcmp(variable, "awb_gain")) res = s->set_awb_gain(s, val);
-    else if(!strcmp(variable, "agc_gain")) res = s->set_agc_gain(s, val);
-    else if(!strcmp(variable, "aec_value")) res = s->set_aec_value(s, val);
-    else if(!strcmp(variable, "aec2")) res = s->set_aec2(s, val);
-    else if(!strcmp(variable, "dcw")) res = s->set_dcw(s, val);
-    else if(!strcmp(variable, "bpc")) res = s->set_bpc(s, val);
-    else if(!strcmp(variable, "wpc")) res = s->set_wpc(s, val);
-    else if(!strcmp(variable, "raw_gma")) res = s->set_raw_gma(s, val);
-    else if(!strcmp(variable, "lenc")) res = s->set_lenc(s, val);
-    else if(!strcmp(variable, "special_effect")) res = s->set_special_effect(s, val);
-    else if(!strcmp(variable, "wb_mode")) res = s->set_wb_mode(s, val);
-    else if(!strcmp(variable, "ae_level")) res = s->set_ae_level(s, val);
-    else {
-        res = -1;
+    // Expecting "on" or "off"
+    if (strncmp(buf, "on", 2) == 0) {
+        live_feed_enabled = true;
+    } else if (strncmp(buf, "off", 3) == 0) {
+        live_feed_enabled = false;
+    } else {
+        return ESP_FAIL;
     }
 
-    if(res){
-        return httpd_resp_send_500(req);
+    return httpd_resp_send(req, "Feed toggled", HTTPD_RESP_USE_STRLEN);
+}
+
+static esp_err_t cmd_handler(httpd_req_t *req){
+    char buf[128];
+    int ret,cmd;
+    ret = httpd_req_recv(req, buf, sizeof(buf));
+    if (ret <= 0) {
+        return ESP_FAIL;
     }
-
-    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
-    return httpd_resp_send(req, NULL, 0);
-}
-
-static esp_err_t status_handler(httpd_req_t *req){
-    static char json_response[1024];
-
-    sensor_t * s = esp_camera_sensor_get();
-    char * p = json_response;
-    *p++ = '{';
-
-    p+=sprintf(p, "\"framesize\":%u,", s->status.framesize);
-    p+=sprintf(p, "\"quality\":%u,", s->status.quality);
-    p+=sprintf(p, "\"brightness\":%d,", s->status.brightness);
-    p+=sprintf(p, "\"contrast\":%d,", s->status.contrast);
-    p+=sprintf(p, "\"saturation\":%d,", s->status.saturation);
-    p+=sprintf(p, "\"special_effect\":%u,", s->status.special_effect);
-    p+=sprintf(p, "\"wb_mode\":%u,", s->status.wb_mode);
-    p+=sprintf(p, "\"awb\":%u,", s->status.awb);
-    p+=sprintf(p, "\"awb_gain\":%u,", s->status.awb_gain);
-    p+=sprintf(p, "\"aec\":%u,", s->status.aec);
-    p+=sprintf(p, "\"aec2\":%u,", s->status.aec2);
-    p+=sprintf(p, "\"ae_level\":%d,", s->status.ae_level);
-    p+=sprintf(p, "\"aec_value\":%u,", s->status.aec_value);
-    p+=sprintf(p, "\"agc\":%u,", s->status.agc);
-    p+=sprintf(p, "\"agc_gain\":%u,", s->status.agc_gain);
-    p+=sprintf(p, "\"gainceiling\":%u,", s->status.gainceiling);
-    p+=sprintf(p, "\"bpc\":%u,", s->status.bpc);
-    p+=sprintf(p, "\"wpc\":%u,", s->status.wpc);
-    p+=sprintf(p, "\"raw_gma\":%u,", s->status.raw_gma);
-    p+=sprintf(p, "\"lenc\":%u,", s->status.lenc);
-    p+=sprintf(p, "\"hmirror\":%u,", s->status.hmirror);
-    p+=sprintf(p, "\"dcw\":%u,", s->status.dcw);
-    p+=sprintf(p, "\"colorbar\":%u", s->status.colorbar);
-    *p++ = '}';
-    *p++ = 0;
-    httpd_resp_set_type(req, "application/json");
-    httpd_resp_set_hdr(req, "Access-Control-Allow-Origin", "*");
-    return httpd_resp_send(req, json_response, strlen(json_response));
-}
-/*
- static esp_err_t index_handler(httpd_req_t *req){
-     httpd_resp_set_type(req, "text/html");
-     String page = "";
-  page += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">\n";
-  page += "<script>var xhttp = new XMLHttpRequest();</script>";
-  page += "<script>function getsend(arg) { xhttp.open('GET', arg +'?' + new Date().getTime(), true); xhttp.send() } </script>";
-  //page += "<p align=center><IMG SRC='http://" + WiFiAddr + ":81/stream' style='width:280px;'></p><br/><br/>";
-  page += "<p align=center><IMG SRC='http://" + WiFiAddr + ":81/stream' style='width:300px; transform:rotate(0deg);'></p><br/><br/>";
- 
-  page += "<p align=center> <button style=background-color:lightgrey;width:90px;height:80px onmousedown=getsend('go') onmouseup=getsend('stop') ontouchstart=getsend('go') ontouchend=getsend('stop') ><b>Forward</b></button> </p>";
-  page += "<p align=center>";
-  page += "<button style=background-color:lightgrey;width:90px;height:80px; onmousedown=getsend('left') onmouseup=getsend('stop') ontouchstart=getsend('left') ontouchend=getsend('stop')><b>Left</b></button>&nbsp;";
-  page += "<button style=background-color:indianred;width:90px;height:80px onmousedown=getsend('stop') onmouseup=getsend('stop')><b>Stop</b></button>&nbsp;";
-  page += "<button style=background-color:lightgrey;width:90px;height:80px onmousedown=getsend('right') onmouseup=getsend('stop') ontouchstart=getsend('right') ontouchend=getsend('stop')><b>Right</b></button>";
-  page += "</p>";
-
-  page += "<p align=center><button style=background-color:lightgrey;width:90px;height:80px onmousedown=getsend('back') onmouseup=getsend('stop') ontouchstart=getsend('back') ontouchend=getsend('stop') ><b>Backward</b></button></p>";  
-
-  page += "<p align=center>";
-  page += "<button style=background-color:yellow;width:140px;height:40px onmousedown=getsend('ledon')><b>Light ON</b></button>";
-  page += "<button style=background-color:yellow;width:140px;height:40px onmousedown=getsend('ledoff')><b>Light OFF</b></button>";
-  page += "</p>";
- 
-     return httpd_resp_send(req, &page[0], strlen(&page[0]));
- }
-*/
-
-
-static esp_err_t index_handler(httpd_req_t *req){
-   httpd_resp_set_type(req, "text/html");
-   String page = "";
-page += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">\n";
-page +="<style>.test{-moz-user-select: none;-o-user-select:none;-khtml-user-select:none;-webkit-user-select:none;-ms-user-select:none;user-select:none;}</style>";
-
-page += "<script>var xhttp = new XMLHttpRequest();</script>";
-page += "<script>function getsend(arg) { xhttp.open('GET', arg +'?' + new Date().getTime(), true); xhttp.send() } </script>";
-//page += "<p align=center><IMG SRC='http://" + WiFiAddr + ":81/stream' style='width:280px;'></p><br/><br/>";
-page += "<p align=center><IMG SRC='http://" + WiFiAddr + ":81/stream' style='width:300px; transform:rotate(0deg);'></p><br/><br/>";
-page +="<div unselectable=\"on\" onselectstart=\"return false\">";
-page += "<p align=center> <button style=background-color:Green;width:90px;height:80px onmousedown=getsend('go') onmouseup=getsend('stop') ontouchstart=getsend('go') ontouchend=getsend('stop') ><b>Forward</b></button> </p>";
-page += "<p align=center>";
-page += "<button style=background-color:Green;width:90px;height:80px; onmousedown=getsend('left') onmouseup=getsend('stop') ontouchstart=getsend('left') ontouchend=getsend('stop')><b>Left</b></button>&nbsp;";
-page += "<button style=background-color:Red;width:90px;height:80px onmousedown=getsend('stop') onmouseup=getsend('stop')><b>Stop</b></button>&nbsp;";
-page += "<button style=background-color:Green;width:90px;height:80px onmousedown=getsend('right') onmouseup=getsend('stop') ontouchstart=getsend('right') ontouchend=getsend('stop')><b>Right</b></button>";
-page += "</p>";
-
-page += "<p align=center><button style=background-color:Green;width:90px;height:80px onmousedown=getsend('back') onmouseup=getsend('stop') ontouchstart=getsend('back') ontouchend=getsend('stop') ><b>Backward</b></button></p>";  
-
-page += "<p align=center>";
-page += "<button style=background-color:yellow;width:140px;height:40px onmousedown=getsend('ledon')><b>Light ON</b></button>";
-page += "<button style=background-color:yellow;width:140px;height:40px onmousedown=getsend('ledoff')><b>Light OFF</b></button>";
-page += "</p>";
-page +="</div>";
-
-   return httpd_resp_send(req, &page[0], strlen(&page[0]));
-}
-
-
-
-
-
-
-
-
-
-
-static esp_err_t go_handler(httpd_req_t *req){
-    //WheelAct(HIGH, LOW, HIGH, LOW);
-    robot_fwd();
-    Serial.println("Go");
-    httpd_resp_set_type(req, "text/html");
-    return httpd_resp_send(req, "OK", 2);
-}
-static esp_err_t back_handler(httpd_req_t *req){
-    //WheelAct(LOW, HIGH, LOW, HIGH);
-    robot_back();
-    Serial.println("Back");
-    httpd_resp_set_type(req, "text/html");
-    return httpd_resp_send(req, "OK", 2);
-}
-
-static esp_err_t left_handler(httpd_req_t *req){
-    //WheelAct(HIGH, LOW, LOW, HIGH);
-    robot_left();
-    Serial.println("Left");
-    httpd_resp_set_type(req, "text/html");
-    return httpd_resp_send(req, "OK", 2);
-}
-static esp_err_t right_handler(httpd_req_t *req){
-    //WheelAct(LOW, HIGH, HIGH, LOW);
-    robot_right();
-    Serial.println("Right");
-    httpd_resp_set_type(req, "text/html");
-    return httpd_resp_send(req, "OK", 2);
-}
-
-static esp_err_t stop_handler(httpd_req_t *req){
-    //WheelAct(LOW, LOW, LOW, LOW);
-    robot_stop();
-    Serial.println("Stop");
-    httpd_resp_set_type(req, "text/html");
-    return httpd_resp_send(req, "OK", 2);
-}
-
-static esp_err_t ledon_handler(httpd_req_t *req){
-    digitalWrite(gpLed, HIGH);
-    Serial.println("LED ON");
-    httpd_resp_set_type(req, "text/html");
-    return httpd_resp_send(req, "OK", 2);
-}
-static esp_err_t ledoff_handler(httpd_req_t *req){
-    digitalWrite(gpLed, LOW);
-    Serial.println("LED OFF");
-    httpd_resp_set_type(req, "text/html");
-    return httpd_resp_send(req, "OK", 2);
+    buf[ret] = '\0';
+    sscanf(buf, "%d", &cmd);
+    Serial.printf("cmd = %d\n", cmd);
+    switch(cmd){
+        case 0: robot_stop(); break;
+        case 1: robot_fwd(); break;
+        case 2: robot_back(); break;
+        case 3: robot_left(); break;
+        case 4: robot_right(); break;
+    }
+    return httpd_resp_send(req, "Command executed", HTTPD_RESP_USE_STRLEN);
 }
 
 void startCameraServer(){
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.server_port = 80;
+    config.ctrl_port = 32768;
 
-    httpd_uri_t go_uri = {
-        .uri       = "/go",
-        .method    = HTTP_GET,
-        .handler   = go_handler,
-        .user_ctx  = NULL
-    };
-
-    httpd_uri_t back_uri = {
-        .uri       = "/back",
-        .method    = HTTP_GET,
-        .handler   = back_handler,
-        .user_ctx  = NULL
-    };
-
-    httpd_uri_t stop_uri = {
-        .uri       = "/stop",
-        .method    = HTTP_GET,
-        .handler   = stop_handler,
-        .user_ctx  = NULL
-    };
-
-    httpd_uri_t left_uri = {
-        .uri       = "/left",
-        .method    = HTTP_GET,
-        .handler   = left_handler,
-        .user_ctx  = NULL
-    };
-    
-    httpd_uri_t right_uri = {
-        .uri       = "/right",
-        .method    = HTTP_GET,
-        .handler   = right_handler,
-        .user_ctx  = NULL
-    };
-    
-    httpd_uri_t ledon_uri = {
-        .uri       = "/ledon",
-        .method    = HTTP_GET,
-        .handler   = ledon_handler,
-        .user_ctx  = NULL
-    };
-    
-    httpd_uri_t ledoff_uri = {
-        .uri       = "/ledoff",
-        .method    = HTTP_GET,
-        .handler   = ledoff_handler,
-        .user_ctx  = NULL
-    };
-
-    httpd_uri_t index_uri = {
-        .uri       = "/",
-        .method    = HTTP_GET,
-        .handler   = index_handler,
-        .user_ctx  = NULL
-    };
-
-    httpd_uri_t status_uri = {
-        .uri       = "/status",
-        .method    = HTTP_GET,
-        .handler   = status_handler,
-        .user_ctx  = NULL
-    };
-
-    httpd_uri_t cmd_uri = {
-        .uri       = "/control",
-        .method    = HTTP_GET,
-        .handler   = cmd_handler,
-        .user_ctx  = NULL
-    };
-
-    httpd_uri_t capture_uri = {
-        .uri       = "/capture",
-        .method    = HTTP_GET,
-        .handler   = capture_handler,
-        .user_ctx  = NULL
-    };
-
-   httpd_uri_t stream_uri = {
+    httpd_uri_t stream_uri = {
         .uri       = "/stream",
         .method    = HTTP_GET,
         .handler   = stream_handler,
         .user_ctx  = NULL
     };
 
+    httpd_uri_t cmd_uri = {
+        .uri       = "/cmd",
+        .method    = HTTP_POST,
+        .handler   = cmd_handler,
+        .user_ctx  = NULL
+    };
 
-    ra_filter_init(&ra_filter, 20);
-    Serial.printf("Starting web server on port: '%d'", config.server_port);
-    if (httpd_start(&camera_httpd, &config) == ESP_OK) {
-        httpd_register_uri_handler(camera_httpd, &index_uri);
-        httpd_register_uri_handler(camera_httpd, &go_uri); 
-        httpd_register_uri_handler(camera_httpd, &back_uri); 
-        httpd_register_uri_handler(camera_httpd, &stop_uri); 
-        httpd_register_uri_handler(camera_httpd, &left_uri);
-        httpd_register_uri_handler(camera_httpd, &right_uri);
-        httpd_register_uri_handler(camera_httpd, &ledon_uri);
-        httpd_register_uri_handler(camera_httpd, &ledoff_uri);
-    }
+    httpd_uri_t toggle_feed_uri = {
+        .uri       = "/toggle_feed",
+        .method    = HTTP_POST,
+        .handler   = toggle_feed_handler,
+        .user_ctx  = NULL
+    };
 
-    config.server_port += 1;
-    config.ctrl_port += 1;
-    Serial.printf("Starting stream server on port: '%d'", config.server_port);
+    // Starting the server
     if (httpd_start(&stream_httpd, &config) == ESP_OK) {
         httpd_register_uri_handler(stream_httpd, &stream_uri);
+        httpd_register_uri_handler(stream_httpd, &cmd_uri);
+        httpd_register_uri_handler(stream_httpd, &toggle_feed_uri);
     }
 }
-
